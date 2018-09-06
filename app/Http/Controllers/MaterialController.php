755b875ab9de64c7297db53011a9bed2f3b67912
kui.chu <kui.chu@yintech.cn>
2018-09-06 09:58:03 +0800

    ## 【09/06 第一次更新】
    1. 完成material api

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Material;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MaterialController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
@@ -18,30 +20,38 @@ class MaterialController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        // 验证数据
+        $request->validate([
+            "title" => "max:255",
+            "url"   => "required|url",
+        ]);
+
+        $material = new Material;
+        $material->title = $request->input("title", "default material title");
+        $material->type = Material::TYPE_IMAGE;
+        $material->status = Material::STATUS_NORMAL;
+        $material->url = $request->input("url");
+        $material->user_id = Auth::id();
+
+        $isSaved = $material->save();
+        if ($isSaved) {
+            return response()->api($material, "素材创建成功!");
+        } else {
+            return response()->api($material, "素材创建失败!", 500);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Material  $material
+     * @param  \App\Material $material
      * @return \Illuminate\Http\Response
      */
     public function show(Material $material)
@@ -52,47 +62,76 @@ class MaterialController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Material  $material
+     * @param  \App\Material $material
      * @return \Illuminate\Http\Response
      */
     public function edit(Material $material)
     {
-        //
+        return response()->api($material);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Material  $material
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Material $material
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Material $material)
     {
-        //
+        // 验证数据
+        $request->validate([
+            "title" => "max:255",
+            "url"   => "required|url",
+        ]);
+
+        $material->title = $request->input("title", "default material title");
+        $material->url = $request->input("url");
+
+        $isSaved = $material->save();
+        if ($isSaved) {
+            return response()->api($material, "素材修改成功!");
+        } else {
+            return response()->api($material, "素材修改失败!", 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Material  $material
+     * @param  \App\Material $material
      * @return \Illuminate\Http\Response
      */
     public function destroy(Material $material)
     {
-        //
+        if ($material->status == Material::STATUS_DELETED) {
+            return response()->api($material, "该素材已经删除，请勿重复操作!");
+        } else {
+            $material->status = Material::STATUS_DELETED;
+            $isDeleted = $material->save();
+            return response()->api($material, "删除成功!");
+        }
     }
 
     /**
+     * 上传文件到七牛云
      * @param Request $request
      * @return \Illuminate\Http\Response
      */
     public function upload(Request $request)
     {
+        $request->validate([
+            "file" => "required|file",
+        ]);
         $path = $request->file("file")->store(
-            'image/'.$request->user()->id, 'qiniu'
+            'image/' . $request->user()->id, 'qiniu'
         );
-        $fullPath = config("filesystems.material_root_url"). $path;
-        echo $fullPath;die;
+        if (!$path) {
+            return response()->api([], "文件保存失败！", 500);
+        } else {
+            $fullPath = config("filesystems.material_root_url") . $path;
+
+            return response()->api(["full_path" => $fullPath], "文件保存成功!");
+        }
     }
 }
--- app/Material.php
+++ app/Material.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Material extends Model
 {
+    const TYPE_IMAGE = 1; // 图片类型
 
+    const STATUS_NORMAL = 1;
+    const STATUS_DELETED = 2;
 }
